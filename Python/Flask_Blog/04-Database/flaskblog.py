eea4dec87e660f5e97647081ffaa9ba2a883d9d0
John Z <zhjohn925@gmail.com>
2021-02-22 16:46:54 -0800

    update

--- Python/Flask_Blog/04-Database/flaskblog.py
+++ Python/Flask_Blog/04-Database/flaskblog.py
@@ -5,7 +5,7 @@ from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site2.db'
 db = SQLAlchemy(app)
 
 
