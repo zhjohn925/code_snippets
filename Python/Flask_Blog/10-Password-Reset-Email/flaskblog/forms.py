5e646eb6fd967aff1a7d2009b94c80129e74e6e5
ridd12 <67840401+ridd12@users.noreply.github.com>
2021-03-13 12:34:03 +0530

    Update forms.py

--- Python/Flask_Blog/10-Password-Reset-Email/flaskblog/forms.py
+++ Python/Flask_Blog/10-Password-Reset-Email/flaskblog/forms.py
@@ -10,7 +10,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                     s=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
